78ff2a1303b0a1625b014bc72a5e72eb13ba43a2
Ayush Sindal <ayush.sindal@coditas.com>
2025-12-16 15:15:09 +0530

    basic layout for the dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dashboard } from "./features/dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
