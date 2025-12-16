ccdf4a2c9df6315ec0b5e01676142daf6d8fb377
Shreelatha Suranagi <shreelatha.suranagi@coditas.com>
2025-12-16 13:37:07 +0530

    Dashboard routes

---
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const DASHBOARD_ROUTES: Routes = [
+  {
+    path: 'family-info',
+    loadComponent: () => import('./pages/family-info/family-info').then((m) => m.FamilyInfo),
+  },
+  {
+    path: 'general-info',
+    loadComponent: () => import('./pages/general-info/general-info').then((m) => m.GeneralInfo),
+  },
+  {
+    path: 'personal-info',
+    loadComponent: () => import('./pages/personal-info/personal-info').then((m) => m.PersonalInfo),
+  },
+];
