9d612b681104979f6aedf9fda14ffbbe68946f0e
Ayush Sindal <ayush.sindal@coditas.com>
2025-12-17 10:01:35 +0530

    updating sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ProgressBar } from './shared/components/progress-bar/progress-bar';
+import { RouterOutlet } from '@angular/router';
+import { Dashboard } from "./features/dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [ProgressBar],
+  imports: [ProgressBar, RouterOutlet, Dashboard],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
