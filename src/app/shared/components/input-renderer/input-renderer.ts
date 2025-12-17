7da6cc9833aa0fb048de909012c386b5b9333ff5
nandini.korukonda-coditas <nandini.korukonda@coditas.com>
2025-12-17 10:20:26 +0530

    Updating Card

--- src/app/shared/components/input-renderer/input-renderer.ts
+++ src/app/shared/components/input-renderer/input-renderer.ts
@@ -1,34 +1,55 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Question } from '../../../core/models/quiz.model';
 
 @Component({
   selector: 'app-input-renderer',
+  standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatRadioModule,
-    MatSelectModule,
     MatCheckboxModule,
   ],
   templateUrl: './input-renderer.html',
   styleUrl: './input-renderer.scss',
 })
 export class InputRenderer {
+  // input signal
   question = input.required<Question>();
-  value = input<any>(null);
 
-  valueChange = output<any>();
+  // local answer state (signal)
+  answer = signal<string | number | string[] | null>(null);
 
-  onChange(value: any) {
-    this.valueChange.emit(value);
+  // output signal
+  answerChange = output<string | number | string[] | null>();
+
+  // TEXT / NUMBER
+  onTextChange(value: string | number) {
+    this.answer.set(value);
+    this.answerChange.emit(this.answer());
+  }
+
+  // RADIO
+  onRadioChange(value: string) {
+    this.answer.set(value);
+    this.answerChange.emit(this.answer());
+  }
+
+  // CHECKBOX
+  onCheckboxChange(option: string, checked: boolean) {
+    const current = (this.answer() as string[]) ?? [];
+
+    const updated = checked
+      ? [...current, option]
+      : current.filter(v => v !== option);
+
+    this.answer.set(updated);
+    this.answerChange.emit(this.answer());
   }
 }
